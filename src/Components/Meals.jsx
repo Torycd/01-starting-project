de891bbc7d36d1ffdddcc88026f7713f0e8ac157
Torycd <vicidundun@gmail.com>
2024-04-05 11:46:00 +0100

    lets do this

---
+++ src/Components/Meals.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import MealItem from "./MealItem";
+
+const Meals = () => {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      try {
+        const response = await fetch("http://localhost:3000/meals");
+        if (!response.ok) {
+          // ...
+        }
+        const meals = await response.json();
+        setLoadedMeals(meals);
+      } catch (error) {
+        // ..
+      }
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal}/>
+      ))}
+    </ul>
+  );
+};
+
+export default Meals;
