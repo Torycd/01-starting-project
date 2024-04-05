c31a7f7b8915b9aa75bd27014929f27a2df36022
Torycd <vicidundun@gmail.com>
2024-04-05 15:06:23 +0100

    lets

--- src/Components/MealItem.jsx
+++ src/Components/MealItem.jsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { currencyFormatter } from "../util/formatting.js";
+import CartContext from "../store/CartContext.jsx";
 
-import React from "react";
 import Button from "./UI/Button.jsx";
 
 const MealItem = ({ meal }) => {
+  const cartCtx = useContext(CartContext);
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
   return (
     <li className="meal-item">
       <article>
@@ -16,7 +21,7 @@ const MealItem = ({ meal }) => {
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <Button>Add to Cart</Button>
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
         </p>
       </article>
     </li>
